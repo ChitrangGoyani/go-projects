473edc0d7a5ccaac4960378dd9ad08030ea6ac0c
ChitrangGoyani <chitranggoyani@gmail.com>
2024-01-29 14:39:54 -0500

    login logout and user retrieval

--- go-jwt-react/controllers/authController.go
+++ go-jwt-react/controllers/authController.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/ChitrangGoyani/go-projects/tree/main/go-jwt-react/database"
 	"github.com/ChitrangGoyani/go-projects/tree/main/go-jwt-react/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var SecretKey = "secret"
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -49,5 +55,62 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	return c.Status(200).JSON(user)
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.ID)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // alive for a day
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(&fiber.Map{
+			"message": "Could not login",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true, // front-end should not be able to access the cookie
+	}
+
+	c.Cookie(&cookie)
+
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Successful Login",
+	})
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(&fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	return c.JSON(user)
+}
+
+// make another cookie and set expiry time to past
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+
+	return c.JSON(&fiber.Map{
+		"message": "Success",
+	})
 }
--- go-jwt-react/routes/routes.go
+++ go-jwt-react/routes/routes.go
@@ -8,4 +8,7 @@ import (
 func Setup(app *fiber.App) {
 	app.Group("/api")
 	app.Post("/register", controllers.Register)
+	app.Post("/login", controllers.Login)
+	app.Get("/user", controllers.User)
+	app.Post("/logout", controllers.Logout)
 }
